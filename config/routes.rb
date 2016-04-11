c0a545f6d7c538d7ac954023056a76c65a52f403
Karson Hatch <karsonhatch@gmail.com>
2016-04-11 14:48:43 -0600

    Added a ton of styling and configured Omniauth for FB

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
   # Custom GET
 
