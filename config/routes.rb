83896174d51cc350432728167604608d98e25a64
Daniel Glover <gloversdaniel@gmail.com>
2016-04-23 11:12:37 -0600

    removed omniauth creds from gitignore for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-
-  get 'users/show'
-
   root "home#index"
   
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
