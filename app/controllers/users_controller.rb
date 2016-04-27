e43411cd11bf918d406e50e27fa21de220007fce
Daniel Glover <gloversdaniel@gmail.com>
2016-04-26 20:45:21 -0600

    added delete button/fixed route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    authorize user
-    user.destroy
+    @user.destroy
     redirect_to users_path, :notice => "User deleted."
   end
 end
